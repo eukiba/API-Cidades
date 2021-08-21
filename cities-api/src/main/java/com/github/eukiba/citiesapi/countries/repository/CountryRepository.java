e6114539e003c566231de01b224e09be95d83243
Renato Nuno <80795209+eukiba@users.noreply.github.com>
2021-08-21 02:43:46 -0300

    Delete cities-api directory

--- cities-api/src/main/java/com/github/eukiba/citiesapi/countries/repository/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.eukiba.citiesapi.countries.repository;
-
-import com.github.eukiba.citiesapi.countries.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
--- cities-api/src/main/java/com/github/eukiba/citiesapi/states/StateRepository.java
+++
@@ -1,6 +0,0 @@
-package com.github.eukiba.citiesapi.states;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StateRepository extends JpaRepository<State, Long> {
-}
